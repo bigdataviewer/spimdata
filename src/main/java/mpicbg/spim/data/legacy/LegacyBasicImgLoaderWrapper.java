eafa5c05405e2f3165697fdef81fab2049fa9b1a
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-09-30 22:45:52 +0200

    add LegacyBasicImgLoaderWrapper

---
+++ src/main/java/mpicbg/spim/data/legacy/LegacyBasicImgLoaderWrapper.java
@@ -0,0 +1,62 @@
+package mpicbg.spim.data.legacy;
+
+import java.util.HashMap;
+
+import mpicbg.spim.data.generic.sequence.BasicImgLoader;
+import mpicbg.spim.data.generic.sequence.BasicSetupImgLoader;
+import mpicbg.spim.data.generic.sequence.ImgLoaderHint;
+import mpicbg.spim.data.sequence.ImgLoader;
+import mpicbg.spim.data.sequence.SetupImgLoader;
+import mpicbg.spim.data.sequence.ViewId;
+import mpicbg.spim.data.sequence.VoxelDimensions;
+import net.imglib2.Dimensions;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.numeric.real.FloatType;
+
+//@Deprecated
+public class LegacyBasicImgLoaderWrapper< T, I extends LegacyBasicImgLoader< T > > implements BasicImgLoader
+{
+	protected final I legacyImgLoader;
+
+	private final HashMap< Integer, SetupImgLoaderWrapper > setupImgLoaders;
+
+	public LegacyBasicImgLoaderWrapper( final I legacyImgLoader )
+	{
+		this.legacyImgLoader = legacyImgLoader;
+		setupImgLoaders = new HashMap< Integer, SetupImgLoaderWrapper >();
+	}
+
+	@Override
+	public synchronized SetupImgLoaderWrapper getSetupImgLoader( final int setupId )
+	{
+		SetupImgLoaderWrapper sil = setupImgLoaders.get( setupId );
+		if ( sil == null )
+		{
+			sil = new SetupImgLoaderWrapper( setupId );
+			setupImgLoaders.put( setupId, sil );
+		}
+		return sil;
+	}
+
+	public class SetupImgLoaderWrapper implements BasicSetupImgLoader< T >
+	{
+		private final int setupId;
+
+		protected SetupImgLoaderWrapper( final int setupId )
+		{
+			this.setupId = setupId;
+		}
+
+		@Override
+		public RandomAccessibleInterval< T > getImage( final int timepointId, final ImgLoaderHint... hints )
+		{
+			return legacyImgLoader.getImage( new ViewId( timepointId, setupId ) );
+		}
+
+		@Override
+		public T getImageType()
+		{
+			return legacyImgLoader.getImageType();
+		}
+	}
+}
